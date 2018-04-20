0941aad6be35ea418101c3422503622a9f670f69
Shkodenko V. Taras <taras@shkodenko.com>
2018-04-20 20:42:47 +0300

    LC #11 contact model

--- controllers/contacts.controller.php
+++ controllers/contacts.controller.php
@@ -1,9 +1,19 @@
 <?php
 
-class ContactsController extends Controller{
-
-    public function index(){
-
+class ContactsController extends Controller
+{
+    public function __construct($data = array())
+    {
+        parent::__construct($data);
+        $this->model = new Contact();
     }
 
+    public function index()
+    {
+        if (!empty($_POST)) {
+            $this->model->save($_POST);
+        }
+
+        echo $this->model->getErrors();
+    }
 }
